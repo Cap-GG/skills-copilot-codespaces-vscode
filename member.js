c13fa12f1e5f14eba76cd233f93db580e2cca527
Cap-GG <guilherme-francisco.gomes@capgemini.com>
2025-03-07 17:25:05 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,14 @@
+function skillsMember() {
+  return {
+    skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Node', 'Express', 'MongoDB'],
+    bio: 'I am a full stack developer.',
+    experience: 'I have been developing websites for 3 years.',
+    education: 'I have a bachelor\'s degree in computer science.',
+    projects: [
+      {
+        name: 'Project 1',
+        description: 'A website for selling books.',
+        technologies: ['HTML', 'CSS', 'JavaScript'],
+        liveDemo: 'https://project1.com',
+          github: ''
+        }
